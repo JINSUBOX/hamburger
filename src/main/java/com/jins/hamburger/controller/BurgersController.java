af8db93dd22ab895c6f9b21bbd85b535c76134cf
Jinsup9k <01077717141p@gmail.com>
2022-07-21 17:39:19 +0900

    add swagger2

--- src/main/java/com/jins/hamburger/controller/BurgersController.java
+++ src/main/java/com/jins/hamburger/controller/BurgersController.java
@@ -5,38 +5,39 @@ import com.jins.hamburger.dto.hamburger.HamburgerResponseDto;
 import com.jins.hamburger.dto.hamburger.HamburgerSaveRequestDto;
 import com.jins.hamburger.dto.hamburger.HamburgerUpdateRequestDto;
 import com.jins.hamburger.service.HamburgerService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@Tag(name = "Burgers", description = "햄버거 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
-public class ApiController {
+@RequestMapping("/api/burgers")
+public class BurgersController {
     private final HamburgerService hamburgerService;
 
     //햄버거 등록 contrller
-    @PostMapping("/burgers")
+    @PostMapping
     public Long save(@RequestBody HamburgerSaveRequestDto requestDto) {
         return hamburgerService.save(requestDto);
     }
 
     //전체 조회 controller
-    @GetMapping("/burgers")
+    @GetMapping
     public List<HamburgerListResponseDto> findAll() {
         return hamburgerService.findAllDesc();
     }
 
     //단일 조회 contoller
-    @GetMapping("/burgers/{id}")
+    @GetMapping("/{id}")
     public HamburgerResponseDto findById(@PathVariable Long id) {
         return hamburgerService.findById(id);
     }
 
     //수정 controller
-    @PutMapping("/burgers/{id}")
+    @PutMapping("/{id}")
     public Long update(
             @PathVariable Long id,
             @RequestBody HamburgerUpdateRequestDto requestDto) {
@@ -44,7 +45,7 @@ public class ApiController {
     }
 
     //삭제 controller
-    @DeleteMapping("/burgers/{id}")
+    @DeleteMapping("/{id}")
     public Long delete(@PathVariable Long id) {
         hamburgerService.delete(id);
         return id;
